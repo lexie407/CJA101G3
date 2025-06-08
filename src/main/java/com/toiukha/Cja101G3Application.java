6824e836a46366ec0dd7aaa11e0893634cec5166
Lexie Lai <lexie160407@gmail.com>
2025-06-08 17:33:14 +0800

    init by lexie

---
+++ src/main/java/com/toiukha/Cja101G3Application.java
@@ -0,0 +1,13 @@
+package com.toiukha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cja101G3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cja101G3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/toiukha/Cja101G3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toiukha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cja101G3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
