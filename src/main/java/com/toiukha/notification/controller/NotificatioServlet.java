06bfd56a2d8b0f36881316e2ae59d81dc72e9133
Jason Huang <virage1257@hotmail.com>
2025-06-10 17:15:37 +0800

    change file name notificationServlet > NotificationServlet

---
+++ src/main/java/com/toiukha/notification/controller/NotificatioServlet.java
@@ -0,0 +1,15 @@
+package com.toiukha.notification.controller;
+
+import java.sql.Timestamp;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+
+@Controller
+public class NotificatioServlet {
+
+	
+	
+}
