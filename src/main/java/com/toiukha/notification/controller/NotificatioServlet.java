dc1175b1e751ae2e8cec10d7ad9e6c8aa9abf2e8
Jason Huang <virage1257@hotmail.com>
2025-06-10 22:13:33 +0800

    update repository and service, add front-end function in servlet

--- src/main/java/com/toiukha/notification/controller/NotificatioServlet.java
+++ src/main/java/com/toiukha/notification/controller/NotificatioServlet.java
@@ -1,15 +1,78 @@
 package com.toiukha.notification.controller;
 
 import java.sql.Timestamp;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.toiukha.notification.model.NotificationService;
+import com.toiukha.notification.model.NotificationVO;
 
 @Controller
+@Validated
+@RequestMapping("/notification")
 public class NotificatioServlet {
 
+	@Autowired
+	NotificationService notificationService;
 	
+	////=====前台=====////
+	//取得該會員的通知資料
+	@PostMapping("getMemNoti")
+	public String getAllNoti(
+		//取得請求資料
+		@RequestParam("memId") String memId, 
+		ModelMap model) {
+		
+		//查詢資料
+		List<NotificationVO> list = notificationService.getMemNoti(Integer.valueOf(memId));
+		
+		//轉交資料
+		model.addAttribute("list", list);
+		return "back-end/notification/notificationofMember";
+	}
 	
+	//顯示通知詳細資訊並更改讀取狀態
+	@PostMapping("readedNoti")
+	public String readedNoti(
+		//取得請求資料
+		@RequestParam("notiId") String notiNo,
+		ModelMap model) {
+		
+		//查詢資料並修改狀態
+		Integer notiId = Integer.valueOf(notiNo);
+		NotificationVO notificationVO = notificationService.getOneNoti(notiId);
+		notificationService.updateNotiStatus(notiId, (byte)1);
+		
+		//轉交資料
+		model.addAttribute("notificationVO", notificationVO);
+		return "back-end/notification/notificationDetail";
+	}
+	
+	//使用者移除通知
+	@PostMapping("removeNotibyMember")
+	public String removeNotibyMember(
+		//取得請求資料
+		@RequestParam("notiId") String notiNo,
+		ModelMap model) {
+		
+		//查詢資料後並刪除資料
+		Integer notiId = Integer.valueOf(notiNo);
+		NotificationVO notificationVO = notificationService.getOneNoti(notiId);
+		notificationService.updateNotiStatus(notiId, (byte)2);
+		
+		//轉交資料
+		model.addAttribute("notificationVO", notificationVO);
+		return "back-end/notification/notificationofMember";
+		
+	}
 }
--- src/main/java/com/toiukha/notification/model/NotificationRepository.java
+++ src/main/java/com/toiukha/notification/model/NotificationRepository.java
@@ -1,9 +1,21 @@
 package com.toiukha.notification.model;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface NotificationRepository extends JpaRepository<NotificationVO, Integer> {
 
+	@Transactional
+	@Modifying
+	@Query(value = "UPDATE NotificationVO as nVO SET nVO.notiStatus = :notiStatus, nVO.notiUpdatedAt = :notiUpdatedAt WHERE nVO.notiId = :notiId", nativeQuery = true)
+	void updateSta(Integer notiId, Byte notiStatus, Timestamp notiUpdatedAt);
 	
+	@Query(value = "FROM NotificationVO WHERE memId = :memId ORDER BY notiSendAt")
+	List<NotificationVO> searchByMemId(Integer memId);
 	
 }
