7ab4323823c5066f2730694e0c68c1dfd63a1881
Jason Huang <virage1257@hotmail.com>
2025-06-21 16:27:08 +0800

    comments API model and controller are done

--- src/main/java/com/toiukha/comments/model/CommentsRepository.java
+++ src/main/java/com/toiukha/comments/model/CommentsRepository.java
@@ -3,11 +3,23 @@ package com.toiukha.comments.model;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CommentsRepository extends JpaRepository<CommentsVO, Integer> {
 
 	@Query(value = "FROM CommentsVO WHERE commArt = :commArt")
 	public List<CommentsVO> getbyArt(Integer commArt);
+	
+	@Transactional
+	@Modifying
+	@Query(value = "UPDATE CommentsVO as cVO SET cVO.commSta = :commSta WHERE cVO.commId = :commId ")
+	public void upadteSta(Integer commId, Byte commSta);
+	
+	@Transactional
+	@Modifying
+	@Query(value = "UPDATE CommentsVO as cVO SET cVO.commCon = :commCon, cVO.commImg = :commImg WHERE cVO.commId = :commId ")
+	public void upadteComm(Integer commId, String commCon, byte[] commImg);
 	
 }
--- src/main/java/com/toiukha/comments/model/CommentsService.java
+++ src/main/java/com/toiukha/comments/model/CommentsService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CommentsService {
@@ -13,6 +14,7 @@ public class CommentsService {
 	private CommentsRepository commentsRepository;
 	
 	//新增、修改、刪除
+	@Transactional
 	public void editOne(CommentsVO commentsVO) {
 		commentsRepository.save(commentsVO);
 	}
@@ -26,6 +28,16 @@ public class CommentsService {
 	//查文章所屬留言
 	public List<CommentsVO> getArtComm(Integer commArt){
 		return commentsRepository.getbyArt(commArt);
+	}
+	
+	//改狀態
+	public void changeSta(Integer commId, Byte commSta) {
+		commentsRepository.upadteSta(commId, commSta);
+	}
+	
+	//改留言內容
+	public void changeComm(Integer commId, String commCon, byte[] commImg) {
+		commentsRepository.upadteComm(commId, commCon, commImg);
 	}
 	
 }
