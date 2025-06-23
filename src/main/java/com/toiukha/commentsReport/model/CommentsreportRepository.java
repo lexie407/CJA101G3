2d0be919f182780ab9d5b3ad7a1fbd11ffd49422
Jason Huang <virage1257@hotmail.com>
2025-06-23 22:20:59 +0800

    commentsReport back-end is done

--- src/main/java/com/toiukha/commentsReport/model/CommentsreportRepository.java
+++ src/main/java/com/toiukha/commentsReport/model/CommentsreportRepository.java
@@ -1,7 +1,13 @@
 package com.toiukha.commentsReport.model;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentsreportRepository extends JpaRepository<CommentsReportVO, Integer> {
-
+	
+	@Query(value = "FROM CommentsReportVO AS cVO WHERE memId = :memId")
+	public List<CommentsReportVO> getbyMem(Integer memId);
+	
 }
