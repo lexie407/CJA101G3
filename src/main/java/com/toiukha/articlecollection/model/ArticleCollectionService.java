d09a3721439cb166e4ff721f7298b30e8a879d72
Jason Huang <virage1257@hotmail.com>
2025-06-27 15:04:44 +0800

    add article collection api function

--- src/main/java/com/toiukha/articlecollection/model/ArticleCollectionService.java
+++ src/main/java/com/toiukha/articlecollection/model/ArticleCollectionService.java
@@ -1,6 +1,7 @@
 package com.toiukha.articlecollection.model;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,12 @@ public class ArticleCollectionService {
 	//查詢會員全部
 	public List<ArticleCollectionVO> getByMem(Integer memId){
 		return articleCollectionRepository.findById_MemId(memId);
+	}
+	
+	//查詢單一
+	public ArticleCollectionVO getOne(ArticleCollectionCompositePrimaryKey aPK) {
+		Optional<ArticleCollectionVO> option = articleCollectionRepository.findById(aPK);
+		return option.orElse(null);
 	}
 	
 	//新增
