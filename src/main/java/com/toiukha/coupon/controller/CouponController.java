947242ef01f43e74b65dd923e64f184d301f4be1
Mark Lai <k120733381@gmail.com>
2025-07-01 09:41:39 +0800

    bug fixes

--- src/main/java/com/toiukha/coupon/controller/CouponController.java
+++ src/main/java/com/toiukha/coupon/controller/CouponController.java
@@ -93,6 +93,9 @@ public class CouponController {
 	protected List<CouponVO> referenceListData_byStoreId(Model model, HttpSession session) {
 		
 		Object storeObj = session.getAttribute("store");
+		if (storeObj == null) {
+			return new ArrayList<>(); // 或回傳 null，看你需求
+		}
 		StoreVO store = (StoreVO) storeObj;
 		int storeId = store.getStoreId();
 		session.setAttribute("storeId", storeId);
